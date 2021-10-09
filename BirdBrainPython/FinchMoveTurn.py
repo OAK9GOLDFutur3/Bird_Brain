2b861e618847fd7f4aab17166b796647b8c84492
Future Reed <futurereed@Futures-MBP.attlocal.net>
2021-10-09 11:07:08 -0700

    Intial commit

---
+++ BirdBrainPython/FinchMoveTurn.py
@@ -0,0 +1,19 @@
+from BirdBrain import Finch
+
+
+bird = Finch()
+
+print("exercise 1") 
+bird.setMove("F",50,100)
+bird.setTurn("R",90,50)
+print("exercise 2")
+bird.setMove("F",50,100)
+bird.setTurn("R",90,50)
+print("exercise 3")
+bird.setMove("F",50,100)
+bird.setTurn("R",90,50)
+print("exercise 4")
+bird.setMove("F",50,100)
+#3
+
+#4
