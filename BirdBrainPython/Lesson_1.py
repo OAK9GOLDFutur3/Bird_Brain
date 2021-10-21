ca428e799bf85a985deddde6ea8fbac611e07f30
Future Reed <futurereed@Futures-MBP.attlocal.net>
2021-10-20 18:19:36 -0700

    add a new file

---
+++ BirdBrainPython/Lesson_1.py
@@ -0,0 +1,26 @@
+from BirdBrain import Finch
+
+
+bird = Finch()
+
+
+print("exercise 1") 
+bird.setMove("F",50,100)
+bird.setTurn("R",90,50)
+'''
+print("exercise 2")
+bird.setMove("F",50,100)
+bird.setMove("B",50,100)
+bird.setMove("F",50,100)
+bird.setMove("B",50,100)
+
+print("exercise 3")
+bird.setMove("F",50,100)
+bird.setTurn("R",90,50)
+'''
+print("exercise 4")
+bird.setMove("F",50,100)
+#3
+
+#4
+'''
